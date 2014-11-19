de29a34e90108a6e4915ca8ddc97b25ae483f338
Chanan Braunstein <chanan.braunstein@pearson.com>
2014-11-19 10:19:07 -0500

    Fixed issue with getting name for per request actors.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -14,16 +14,19 @@ public class Application extends Controller {
 	private final ServiceThatUsesActor serviceThatUsesActor;
 	private final ActorRef annotatedActor;
     private final ActorRef abstractActorService;
+    private final ActorRef perRequest;
 	
 	@Inject
 	public Application(@Named("services.HelloActor") ActorRef service, 
 			ServiceThatUsesActor serviceThatUsesActor,
 			@Named("AnnotatedActor") ActorRef annotatedActor,
-            @Named("AbstractActorService") ActorRef abstractActorService) {
+            @Named("AbstractActorService") ActorRef abstractActorService,
+            @Named("PerRequest") ActorRef perRequest) {
 		this.service = service;
 		this.serviceThatUsesActor = serviceThatUsesActor;
 		this.annotatedActor = annotatedActor;
         this.abstractActorService = abstractActorService;
+        this.perRequest = perRequest;
 	}
 	
     public Result index() {
@@ -31,6 +34,7 @@ public class Application extends Controller {
     	serviceThatUsesActor.speak();
     	annotatedActor.tell("hi there", ActorRef.noSender());
         abstractActorService.tell("test", ActorRef.noSender());
+        perRequest.tell("test", ActorRef.noSender());
         return ok(index.render());
     }
 }
